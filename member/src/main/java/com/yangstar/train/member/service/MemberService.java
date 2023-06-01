c978a6433cbffd160342c65b19518768e9b92080
yangstar <goodmale@qq.com>
2023-06-01 23:16:06 +0800

    使用mybatis官方生成器,自动生成很多东西

--- member/src/main/java/com/yangstar/train/member/service/MemberService.java
+++ member/src/main/java/com/yangstar/train/member/service/MemberService.java
@@ -15,6 +15,6 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
     public int count() {
-         return memberMapper.count();
+         return Math.toIntExact(memberMapper.countByExample(null));
     }
 }
